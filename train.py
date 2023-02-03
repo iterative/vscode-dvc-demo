cadde0ace2248a8638e0163b1fcfab66ef4cfecb
Matt Seddon <mattseddon@hotmail.com>
2023-02-04 10:56:36 +1100

    migrate to v2 of dvclive

--- train.py
+++ train.py
@@ -128,7 +128,7 @@ def main():
         torch.nn.Dropout(0.1),
         torch.nn.Linear(64, 10),
     )
-    live = Live("training", report=None)
+    live = Live(dir="training",dvcyaml=False, report=None)
 
     # Load model.
     if os.path.exists("model.pt"):
